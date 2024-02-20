ea018813950aea248de266b4020c05acc2b0c51d
hughjere <hughjere@OA-S143-05>
2024-02-20 17:42:25 -0500

    Commit 3; adding my character and enemy sprites, also using the animator to have my animations work

---
+++ Assets/Assignment/Scripts/Player.cs
@@ -0,0 +1,82 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Apple;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+
+public class Player : MonoBehaviour
+{
+    Rigidbody2D rb;
+    Animator animator;
+    Vector2 destination;
+    Vector2 movement;
+    public float speed = 3;
+    public float health;
+    public float maxHealth = 5;
+    bool isDead;
+    public HealthBar healthBar;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        healthBar = GetComponent<HealthBar>();
+        isDead = false;
+        health = PlayerPrefs.GetFloat("currentHealth", maxHealth);
+        SendMessage("InitializeHealth", health);
+        PlayerPrefs.SetFloat("currentHealth", health);
+    }
+
+    private void FixedUpdate()
+    {
+        if (isDead) return;
+        movement = destination - (Vector2)transform.position;
+        if (movement.magnitude < 0.1)
+        {
+            movement = Vector2.zero;
+        }
+        rb.MovePosition(rb.position + movement.normalized * speed * Time.deltaTime);
+    }
+
+    void Update()
+    {
+        if (isDead) return;
+
+        float movementMagnitude = movement.magnitude;
+        animator.SetFloat("Movement", movementMagnitude);
+
+        if (Input.GetMouseButtonDown(0) && !EventSystem.current.IsPointerOverGameObject())
+        {
+            destination = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+
+        if (Input.GetMouseButton(1))
+        {
+            animator.SetTrigger("Attack");
+        }
+    }
+
+    public void TakeDamage(float damage)
+    {
+        health -= damage;
+        health = Mathf.Clamp(health, 0, maxHealth);
+        SendMessage("UpdateHealthUI", health);
+
+        if (health <= 0)
+        {
+            isDead = true;
+            animator.SetTrigger("Death");
+            SceneManager.LoadScene("Game over page");
+        }
+        else
+        {
+            isDead = false;
+            animator.SetTrigger("Take Damage");
+        }
+        PlayerPrefs.SetFloat("currentHealth", health);
+    }
+}
